041a956a45259ccca00f3a35cc337b44cd973df0
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2024-10-24 10:11:49 +0530

    Time: 2 ms (12.93%), Space: 42 MB (9.78%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,14 +1,26 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int count=0;
+        HashMap<Character,Integer> hm=new HashMap<>();
         for(int i=0;i<stones.length();i++){
-            for(int j=0;j<jewels.length();j++){
-                if(stones.charAt(i)==jewels.charAt(j)){
-                    count++;
-                }
+            char ch=stones.charAt(i);
+            if(hm.containsKey(ch)){
+                int curr=hm.get(ch);
+                hm.put(ch,curr+1);
+
+            }
+            else{
+                hm.put(ch,1);
             }
         }
-        return count;
+        int ans=0;
+        for(int i=0;i<jewels.length();i++){
+            char cc=jewels.charAt(i);
+            if(hm.containsKey(cc)){
+                ans+=hm.get(cc);
+
+            }
+        }
+        return ans;
         
     }
 }
