342ed4b558242bbd5ba3d5aa5a7c86a9266ec373
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2024-12-23 15:40:13 +0530

    Time: 0 ms (100%), Space: 17.9 MB (5.72%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for i in t:
+            if s.count(i)!=t.count(i):
+                return i
+            if i in s:
+                continue
+            else:
+                return i
+        
