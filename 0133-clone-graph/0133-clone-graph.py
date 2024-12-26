c48da486021a84316679708c384301b97c9b9dbd
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2024-12-26 11:54:19 +0530

    Time: 46 ms (20.14%), Space: 18.5 MB (5.22%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,15 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+import copy
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        v=copy.deepcopy(node)
+        return v
+        
