0b8b5b2a2e0b8255fd0fb015ea90426be7825b40
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2025-04-29 11:33:10 +0530

    Time: 8 ms (79.1%), Space: 17.9 MB (64.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         d=Counter(s)
-        d1=Counter(t)
-        return True if d==d1 else False
+        f=Counter(t)
+        return True if d==f else False
