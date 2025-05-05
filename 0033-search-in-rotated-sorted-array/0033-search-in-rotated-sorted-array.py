9e22e68f72ded8f5bbd362d8db39482b656ced62
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2025-05-05 20:43:06 +0530

    Time: 0 ms (100%), Space: 18 MB (67.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,14 +4,13 @@ class Solution:
         sor.extend(nums)
         sor.sort()
         low=0
-        high=len(nums)-1
+        high=len(sor)-1
         while low<=high:
             mid=(low+high)//2
-            if target>sor[mid]:
-                low=mid+1
-            elif target<sor[mid]:
-                high=mid-1
-            elif target==sor[mid]:
+            if sor[mid]==target:
                 return nums.index(target)
+            elif sor[mid]>target:
+                high=mid-1
+            elif sor[mid]<target:
+                low=mid+1
         return -1
-        
