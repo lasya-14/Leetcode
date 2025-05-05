1525b53fb43012c47c9fdec71107841b50027d1b
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2025-05-05 09:30:53 +0530

    Time: 20 ms (94.66%), Space: 33 MB (41.43%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,36 +5,16 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return head
-        
-        # Find the middle of the linked list 
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        mid = slow.next
-        slow.next = None
-        
-        # Sort both halves
-        left = self.sortList(head)
-        right = self.sortList(mid)
-        
-        return self.merge(left, right)
-    
-    def merge(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode(0)
-        tail = dummy
-        
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-        
-        tail.next = l1 if l1 else l2
-        return dummy.next
+        li=[]
+        temp=head
+        while temp:
+            li.append(temp.val)
+            temp=temp.next
+        li.sort()
+        temp=head
+        i=0
+        while temp:
+            temp.val=li[i]
+            temp=temp.next
+            i+=1
+        return head
