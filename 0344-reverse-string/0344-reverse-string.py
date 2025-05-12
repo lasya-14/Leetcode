819399cc7b5c08c5919b4536ad9f4c9e917725f3
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2025-05-12 19:14:07 +0530

    Time: 0 ms (100%), Space: 23 MB (42.25%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,6 +3,5 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        st=list(s)
-        for i in range(len(st)):
-            s[i]=st.pop()
+        
+        s.reverse()
