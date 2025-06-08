d9f1ccfb9a75f097aa9c0b10540f2d9d0afbb6b6
lasya-14 <158994556+lasya-14@users.noreply.github.com>
2025-06-08 16:21:31 +0530

    Time: 3 ms (83.29%), Space: 19.3 MB (87.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        dici={}
+        for i in nums:
+            if i not in dici:
+                dici[i]=1
+            else:
+                dici[i]+=1
+        n=len(nums)
+        for i in dici:
+            if dici[i]>(n//2):
+                return i
+
+
